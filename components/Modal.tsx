97442a9cc828a28b53c7902c16221f14b0cc2f5f
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-08-23 08:32:03 +0700

    Fix import statement for framer-motion

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
